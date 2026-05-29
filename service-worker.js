d634a70af6264f49e385400d73d303820e5d5b52
beJond <160001576+jonasdoer@users.noreply.github.com>
2026-05-29 08:09:24 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,17 +1,20 @@
-const CACHE_NAME = 'wildliste-feature-update-v8';
+const CACHE_NAME = 'wildliste-stable-feature-fix-2026-05-29-v1';
 const ASSETS = ['./', './index.html', './manifest.json'];
 self.addEventListener('install', event => {
   self.skipWaiting();
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS).catch(()=>{})));
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
 });
 self.addEventListener('activate', event => {
-  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+  event.waitUntil(Promise.all([
+    self.clients.claim(),
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))
+  ]));
 });
 self.addEventListener('fetch', event => {
   if (event.request.method !== 'GET') return;
   event.respondWith(fetch(event.request).then(response => {
-    const copy = response.clone();
-    caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy)).catch(()=>{});
+    const clone = response.clone();
+    caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
     return response;
-  }).catch(() => caches.match(event.request)));
+  }).catch(() => caches.match(event.request).then(cached => cached || caches.match('./index.html'))));
 });
