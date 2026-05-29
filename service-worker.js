07c968e4d8fbc14221ea417b1801ab1a0def9a04
beJond <160001576+jonasdoer@users.noreply.github.com>
2026-05-29 08:02:45 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,17 @@
-const CACHE_NAME = 'wildliste-robust-dropdown-1';
-const ASSETS = ['./index.html?v=robust-dropdown-1','./manifest.json'];
-self.addEventListener('install', event => { self.skipWaiting(); event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS).catch(()=>{}))); });
-self.addEventListener('activate', event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))); self.clients.claim(); });
-self.addEventListener('fetch', event => { event.respondWith(fetch(event.request).catch(() => caches.match(event.request).then(r => r || caches.match('./index.html?v=robust-dropdown-1')))); });
+const CACHE_NAME = 'wildliste-feature-update-v8';
+const ASSETS = ['./', './index.html', './manifest.json'];
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS).catch(()=>{})));
+});
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+});
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(fetch(event.request).then(response => {
+    const copy = response.clone();
+    caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy)).catch(()=>{});
+    return response;
+  }).catch(() => caches.match(event.request)));
+});
